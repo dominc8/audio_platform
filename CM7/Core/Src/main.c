e7aca002b75f5dfa42781983748f57d51521ec7f
dominc8 <kormandominik@gmail.com>
2021-02-06 20:25:33 +0100

    Remove SD demos to check SDRAM, add notes

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -61,7 +61,7 @@ static void Display_DemoDescription(void);
 static void MPU_Config(void);
 static void CPU_CACHE_Enable(void);
 
-BSP_DemoTypedef  BSP_examples[]=
+BSP_DemoTypedef BSP_examples[]=
 {
   {Joystick_demo, "JOYSTICK EXTI", 0},
   {Touchscreen_demo1, "TOUCHSCREEN DEMO1", 0},
@@ -70,10 +70,6 @@ BSP_DemoTypedef  BSP_examples[]=
   {Camera_demo, "CAMERA", 0},
   {AudioPlay_demo, "AUDIO PLAY", 0},
   {AudioRecord_demo, "AUDIO RECORD", 0},
-  {SD_DMA_demo, "SD", 0},
-  {SD_IT_demo, "SD", 0},
-  {SD_POLLING_demo, "SD", 0},
-  {QSPI_demo, "QSPI", 0},
   {SDRAM_demo, "SDRAM", 0},
   {SDRAM_DMA_demo, "SDRAM MDMA", 0},
 
