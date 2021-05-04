e8c49b62432a561782fbb05d6c4a493b5ce879cb
dominc8 <kormandominik@gmail.com>
2021-05-04 10:11:55 +0200

    Clean fft_16hist_test

--- Test/fft_16hist/fft_16hist_test.c
+++ Test/fft_16hist/fft_16hist_test.c
@@ -16,29 +16,16 @@ void fft_16hist_test(void)
     int16_t yr[Y_SIZE];
 
     load_array(x, sizeof(*x), X_SIZE, "x.dat");
-//     printf("x:\n");
-//     print_array(x, X_SIZE);
-
     load_array(yl_ref, sizeof(*yl_ref), Y_SIZE, "yl.dat");
-//     printf("yl_ref:\n");
-//     print_array(yl_ref, Y_SIZE);
-
     load_array(yr_ref, sizeof(*yr_ref), Y_SIZE, "yr.dat");
-//     printf("yr_ref:\n");
-//     print_array(yr_ref, Y_SIZE);
 
     fft_16hist(yl, yr, x);
 
-//     printf("yl:\n");
-//     print_array(yl, Y_SIZE);
-
-//     printf("yr:\n");
-//     print_array(yr, Y_SIZE);
-
+    int16_t tol = 1;
     for (int32_t i = 0; i < Y_SIZE; ++i)
     {
-        ASSERT_EQUAL_WITH_TOL(yl[i], yl_ref[i], 1);
-        ASSERT_EQUAL_WITH_TOL(yr[i], yr_ref[i], 1);
+        ASSERT_EQUAL_WITH_TOL(yl[i], yl_ref[i], tol);
+        ASSERT_EQUAL_WITH_TOL(yr[i], yr_ref[i], tol);
     }
 }
 
@@ -46,3 +33,4 @@ void fft_16hist_test(void)
     CUNIT_CI_TEST(fft_16hist_test)
 
 CUNIT_CI_RUN("fft_16hist_test", TESTS);
+
