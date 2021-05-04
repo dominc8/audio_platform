ff0765b2d2f0be77c5b130512836d77e83f4c65a
dominc8 <kormandominik@gmail.com>
2021-05-04 09:55:05 +0200

    Add cunit framework

--- Test/fft_16hist/fft_16hist_test.c
+++ Test/fft_16hist/fft_16hist_test.c
@@ -1,8 +1,14 @@
 #include "fft_hist.h"
+#include "CUnit/CUnitCI.h"
+
 #include <stdio.h>
 
 #define X_SIZE 64
 #define Y_SIZE 16
+
+#define ABS_DIFF(x, y) ((x) > (y) ? ((x) - (y)) : ((y) - (x)))
+
+#define ASSERT_EQUAL_WITH_TOL(a, b, tol) CU_ASSERT_TRUE(ABS_DIFF(a, b) <= tol)
 
 int32_t load_array(void *arr, int32_t el_size, int32_t n, const char* filename)
 {
@@ -35,8 +41,7 @@ void print_array(int16_t *arr, int32_t size)
 }
 
 
-
-int main(void)
+void fft_16hist_test(void)
 {
     int16_t x[X_SIZE];
     int16_t yl_ref[Y_SIZE];
@@ -45,24 +50,33 @@ int main(void)
     int16_t yr[Y_SIZE];
 
     load_array(x, sizeof(*x), X_SIZE, "x.dat");
-    printf("x:\n");
-    print_array(x, X_SIZE);
+//     printf("x:\n");
+//     print_array(x, X_SIZE);
 
     load_array(yl_ref, sizeof(*yl_ref), Y_SIZE, "yl.dat");
-    printf("yl_ref:\n");
-    print_array(yl_ref, Y_SIZE);
+//     printf("yl_ref:\n");
+//     print_array(yl_ref, Y_SIZE);
 
     load_array(yr_ref, sizeof(*yr_ref), Y_SIZE, "yr.dat");
-    printf("yr_ref:\n");
-    print_array(yr_ref, Y_SIZE);
+//     printf("yr_ref:\n");
+//     print_array(yr_ref, Y_SIZE);
 
     fft_16hist(yl, yr, x);
 
-    printf("yl:\n");
-    print_array(yl, Y_SIZE);
+//     printf("yl:\n");
+//     print_array(yl, Y_SIZE);
 
-    printf("yr:\n");
-    print_array(yr, Y_SIZE);
+//     printf("yr:\n");
+//     print_array(yr, Y_SIZE);
 
+    for (int32_t i = 0; i < Y_SIZE; ++i)
+    {
+        ASSERT_EQUAL_WITH_TOL(yl[i], yl_ref[i], 1);
+        ASSERT_EQUAL_WITH_TOL(yr[i], yr_ref[i], 1);
+    }
 }
 
+#define TESTS           \
+    CUNIT_CI_TEST(fft_16hist_test)
+
+CUNIT_CI_RUN("fft_16hist_test", TESTS);
