a8767c55013e6982d4c6f77f269cc81e5baa2da6
dominc8 <kormandominik@gmail.com>
2021-05-04 10:05:11 +0200

    Add utils subdir, lib and makefile

--- Test/fft_16hist/fft_16hist_test.c
+++ Test/fft_16hist/fft_16hist_test.c
@@ -1,45 +1,11 @@
 #include "fft_hist.h"
 #include "CUnit/CUnitCI.h"
+#include "utils.h"
 
 #include <stdio.h>
 
 #define X_SIZE 64
 #define Y_SIZE 16
-
-#define ABS_DIFF(x, y) ((x) > (y) ? ((x) - (y)) : ((y) - (x)))
-
-#define ASSERT_EQUAL_WITH_TOL(a, b, tol) CU_ASSERT_TRUE(ABS_DIFF(a, b) <= tol)
-
-int32_t load_array(void *arr, int32_t el_size, int32_t n, const char* filename)
-{
-    FILE *f = fopen(filename, "rb");
-    if (NULL == f)
-        return 0;
-    int32_t n_read = fread(arr, el_size, n, f);
-    fclose(f);
-    return n_read;
-}
-
-void print_farray(float *arr, int32_t size)
-{
-    int32_t i;
-    for (i = 0; i < size; ++i)
-    {
-        printf("%f, ", arr[i]);
-    }
-    printf("\n");
-}
-
-void print_array(int16_t *arr, int32_t size)
-{
-    int32_t i;
-    for (i = 0; i < size; ++i)
-    {
-        printf("%d, ", arr[i]);
-    }
-    printf("\n");
-}
-
 
 void fft_16hist_test(void)
 {
