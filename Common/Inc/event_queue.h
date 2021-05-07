00c83abc97dc6e89c7dfda6981beb44cb8483f4b
dominc8 <kormandominik@gmail.com>
2021-05-07 18:10:42 +0200

    Init event_queue component and testenv

---
+++ Common/Inc/event_queue.h
@@ -0,0 +1,25 @@
+#ifndef EVENT_QUEUE_H
+#define EVENT_QUEUE_H
+
+#include <assert.h>
+#include <stdint.h>
+
+typedef enum EVENT_ID
+{
+    EVENT_M7_TRACE = 0, EVENT_N
+} EVENT_ID;
+
+static_assert(sizeof(EVENT_ID) == 4, "EVENT_ID should be 4 bytes big as it is used for casting array of structs on uint8_t data buffer");
+
+typedef struct event
+{
+    EVENT_ID id;
+    uint32_t val;
+} event;
+
+int32_t eq_m7_init(void);
+int32_t eq_m7_add_event(EVENT_ID id, uint32_t val);
+
+
+#endif /* EVENT_QUEUE_H */
+
---
+++ Test/event_queue/event_queue.c
@@ -0,0 +1,7 @@
+#include "event_queue.h"
+
+int32_t eq_m7_init(void)
+{
+    return 0;
+}
+
