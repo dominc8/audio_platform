2b2c6f2ffd0ca18edf4274dd6fb824112de5adb5
dominc8 <kormandominik@gmail.com>
2021-07-30 15:36:08 +0200

    UT comparing fir_f32 with CMSIS-DSP

--- Test/Stubs/arm_math.h
+++ Test/Stubs/arm_math.h
@@ -2,6 +2,30 @@
 #define ARM_MATH_H
 
 #include "math.h"
+#include "stdint.h"
+#include "string.h"
+
+typedef float float32_t;
+
+typedef struct
+{
+uint16_t numTaps;     /**< number of filter coefficients in the filter. */
+float32_t *pState;    /**< points to the state variable array. The array is of length numTaps+blockSize-1. */
+float32_t *pCoeffs;   /**< points to the coefficient array. The array is of length numTaps. */
+} arm_fir_instance_f32;
+
+void arm_fir_f32(
+const arm_fir_instance_f32 * S,
+float32_t * pSrc,
+float32_t * pDst,
+uint32_t blockSize);
+
+void arm_fir_init_f32(
+arm_fir_instance_f32 * S,
+uint16_t numTaps,
+float32_t * pCoeffs,
+float32_t * pState,
+uint32_t blockSize);
 
 #endif /* ARM_MATH_H */
 
--- Test/fir/fir_test.c
+++ Test/fir/fir_test.c
@@ -1,6 +1,7 @@
 #include "fir.h"
 #include "CUnit/CUnitCI.h"
 #include "utils.h"
+#include "arm_math.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -27,15 +28,56 @@ void fir_test(void)
         y[i] = fir_f32(&f, x[i]);
     }
 
-    int32_t tol = 2;
+    int32_t tol = 1;
     for (int32_t i = 0; i < N; ++i)
     {
         ASSERT_EQUAL_WITH_TOL(y_ref[i], y[i], tol);
     }
 }
 
+void fir_test_arm(void)
+{
+    int32_t n_coeff;
+    int32_t x[N];
+    int32_t y_ref[N];
+    int32_t y[N];
+    float arm_coeff[MAX_FIR_ORDER + 1];
+    float arm_state[MAX_FIR_ORDER + 1];
+    fir_f32_t f;
+    arm_fir_instance_f32 arm_f;
+
+    load_array(x, sizeof(*x), N, "x.dat");
+
+    memset(&f.samples[0], 0, sizeof(f.samples));
+    n_coeff = load_array(&f.coeff[0], sizeof(*(f.coeff)), MAX_FIR_ORDER + 1, "coeff.dat");
+    f.order = n_coeff - 1;
+
+    for (int32_t i = 0; i < n_coeff; ++i)
+    {
+        arm_coeff[i] = f.coeff[n_coeff - 1 - i];
+    }
+    arm_fir_init_f32(&arm_f, n_coeff, &arm_coeff[0], &arm_state[0], 1);
+
+    for (int32_t i = 0; i < N; ++i)
+    {
+        float src = (float) x[i];
+        float dst;
+        arm_fir_f32(&arm_f, &src, &dst, 1);
+        y_ref[i] = (int32_t) dst;
+        y[i] = fir_f32(&f, x[i]);
+    }
+
+    int32_t tol = 1;
+    for (int32_t i = 0; i < N; ++i)
+    {
+        ASSERT_EQUAL_WITH_TOL(y_ref[i], y[i], tol);
+    }
+}
+
+
 #define TESTS           \
-    CUNIT_CI_TEST(fir_test)
+    CUNIT_CI_TEST(fir_test), \
+    CUNIT_CI_TEST(fir_test_arm)
 
 CUNIT_CI_RUN("fir_test", TESTS);
 
